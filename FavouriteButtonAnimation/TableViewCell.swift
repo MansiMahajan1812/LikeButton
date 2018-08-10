7320ed00c041326a2149b90371692ff1aa0ac654
Mansi Mahajan <Mansi@Amrits-Mac-Mini.local>
2018-08-10 09:43:17 +0530

    master

---
+++ FavouriteButtonAnimation/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  FavouriteButtonAnimation
+//
+//  Created by Mansi Mahajan on 8/8/18.
+//  Copyright © 2018 Mansi Mahajan. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
