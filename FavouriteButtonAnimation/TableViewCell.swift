3bb3755be07f2cbae7169838160fe5246656bd1f
Mansi Mahajan <Mansi@Amrits-Mac-Mini.local>
2018-08-29 10:42:42 +0530

    changed

--- FavouriteButtonAnimation/TableViewCell.swift
+++ FavouriteButtonAnimation/TableViewCell.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+func color(_ rgbColor: Int) -> UIColor{
+    return UIColor(
+        red:   CGFloat((rgbColor & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbColor & 0x00FF00) >> 8 ) / 255.0,
+        blue:  CGFloat((rgbColor & 0x0000FF) >> 0 ) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
 
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var likeButton: FaveButton!
+    let colors = [
+        DotColors(first: color(0x7DC2F4), second: color(0xE2264D)),
+        DotColors(first: color(0xF8CC61), second: color(0x9BDFBA)),
+        DotColors(first: color(0xAF90F4), second: color(0x90D1F9)),
+        DotColors(first: color(0xE9A966), second: color(0xF8C852)),
+        DotColors(first: color(0xF68FA7), second: color(0xF6A2B8))
+    ]
+    
+    func faveButton(_ faveButton: FaveButton, didSelected selected: Bool){
+        
+    }
+    
+    func faveButtonDotColors(_ faveButton: FaveButton) -> [DotColors]?{
+        if( faveButton === likeButton ){
+            return colors
+        }
+        return nil
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- FavouriteButtonAnimation/ViewController.swift
+++ FavouriteButtonAnimation/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var boolVariable: Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vc = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        //boolVariable == true ? false: true
+        if indexPath.row % 2 == 0
+        {
+            vc.likeButton.isSelected = true
+        }
+        else{
+            boolVariable = false
+            vc.likeButton.isSelected = false
+        }
+        //vc.likeButton.isSelected = true
+        return vc
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
 }
 
